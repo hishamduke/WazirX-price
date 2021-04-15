6b7f61028f8c57550e65df24d10d3c8a5492bf03
hishamduke <31735889+hishamduke@users.noreply.github.com>
2021-04-15 18:10:33 +0530

    Update pricecomp.py

--- pricecomp.py
+++ pricecomp.py
@@ -2,19 +2,28 @@
 import requests
 import json
 import sys
-val = sys.argv[1]
-coin = sys.argv[2]
+
+coin = sys.argv[1]
+val = sys.argv[2]
+
 url="https://api.wazirx.com/api/v2/trades?market={0}".format(coin)
+
 response = requests.get(url).json()
+
 for obj in response:
     price = obj['price']
     break
-print(f'{coin} wazirx price is {price}')
+
+    print(f'{coin} wazirx price is {price}')
 
 url="https://api.wazirx.com/api/v2/trades?market=usdtinr"
+
 response = requests.get(url).json()
+
 for obj in response:
     price = obj['price']
     break
+
 ogval = float(price) * float(val)
+
 print(f'binance price is {ogval}')
