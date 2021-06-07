924346ba8d8d9a51a6ed4aa323287c4216894d46
hishamduke <31735889+hishamduke@users.noreply.github.com>
2021-06-07 21:37:05 +0530

    Update price2.py

--- price2.py
+++ price2.py
@@ -2,54 +2,30 @@
 import requests
 import json
 import sys
-import cryptocompare
-
-coin = sys.argv[1]
-try:
-    choice = sys.argv[2]
-except:
-    choice = 'i'
-cryptprice = cryptocompare.get_price(coin, currency="USDT")
-
-temp = str(cryptprice.values())
-a=temp.split()
-b=str(a[1][:-3])
-
-if choice == 'i':
-    coin = coin.lower()+"inr"
-elif choice == 'u':
-    coin = coin.lower()+"usdt"
-
-val = b
-
-url="https://api.wazirx.com/api/v2/trades?market={0}".format(coin)
-
-response = requests.get(url).json()
-
-for obj in response:
-    price = obj['price']
-    break
-
-print()
-print(f'__________ {coin} __________')
-print()
-print(f'wazirx price   :  {price}')
-wrx = price
-
-price=1
-
-if choice == 'i':
-    url="https://api.wazirx.com/api/v2/trades?market=usdtinr"
+price = 2
+def getp(arg):
+    url="https://api.wazirx.com/api/v2/trades?market={0}".for>
     response = requests.get(url).json()
     for obj in response:
         price = obj['price']
         break
+    return price
 
-ogval = float(price) * float(val)
+val = sys.argv[2]
+coin = sys.argv[1]
+wrx = getp(coin)
+
+print()
+print(f'__________ {coin} __________')
+print()
+print(f'wazirx price   :  {wrx}')
+
+usdt = getp('usdtinr')
+ogval = float(usdt) * float(val)
+
 perc=((float(wrx)-ogval)/ogval)*100
 
 print(f'binance price  :  {ogval}')
-print('difference      : '+str(perc)[:7]+'%')
+print('difference     :  '+str(perc)[:7]+'%')
 print('____________________________')
 print()
-
